dde73be17bcaaf624eacebc13b08327b484522cb
Sam Daived <37539198+samdaived@users.noreply.github.com>
2026-07-02 20:47:26 +0300

    Correct SUPABASE_PUBLISHABLE_KEY declaration
    Fix variable declaration for SUPABASE_PUBLISHABLE_KEY and add console log.

--- src/integrations/supabase/client.ts
+++ src/integrations/supabase/client.ts
@@ -3,10 +3,11 @@ import { createClient } from '@supabase/supabase-js';
 import type { Database } from './types';
 
 const SUPABASE_URL = import.meta.env.VITE_SUPABASE_URL;
-const SUPABASE_PUBLISHABLE_KEY = import.meta.env.VITE_SUPABASE_PUBLISHABLE_KEY;
+const  = import.meta.env.VITE_SUPABASE_PUBLISHABLE_KEY;
 
 // Import the supabase client like this:
 // import { supabase } from "@/integrations/supabase/client";
+console.log(SUPABASE_URL,SUPABASE_PUBLISHABLE_KEY)
 
 export const supabase = createClient<Database>(SUPABASE_URL, SUPABASE_PUBLISHABLE_KEY, {
   auth: {
